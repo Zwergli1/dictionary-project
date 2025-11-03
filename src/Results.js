3608f1f835024f4dd86edc2f5d191365d7f05d9a
Zwergli1 <54478681+Zwergli1@users.noreply.github.com>
2025-11-03 21:27:47 +0100

    ämid loop

---
+++ src/Results.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Results(props) {
+  console.log(props.results);
+  if (props.results) {
+    return (
+      <div className="Results">
+        <h2>{props.results.word}</h2>
+        {props.results.meanings.map(function(meanings, index)) {
+            return meanings.definition[0]
+        }}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
