0c0cfe03a44da8c2569de0bbad8636bfda751094
Zwergli1 <54478681+Zwergli1@users.noreply.github.com>
2025-11-09 17:46:35 +0100

    fixing examples

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Synonyms from "./Synonyms";
-import Examples from "./Examples";
 
 export default function Meaning(props) {
   console.log(props.meaning);
@@ -9,7 +8,7 @@ export default function Meaning(props) {
       <h3>{props.meaning.partOfSpeech}</h3>
       <strong>Definition:</strong> <p>{props.meaning.definition}</p>
       <p>
-        <strong>Example:</strong> {props.meaning.example}
+        <strong>Example:</strong> <em>{props.meaning.example}</em>
       </p>
       <strong>Synonym(s):</strong>
       <Synonyms synonyms={props.meaning.synonyms} />
